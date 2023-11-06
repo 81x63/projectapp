5c719a109ee5f6cc1c99952bd0c2a0942408aff1
Kouta219 <104189576+Kouta219@users.noreply.github.com>
2023-11-07 03:10:18 +0900

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ["bootstrap/dist/css/bootstrap.min.css",
+        "bootstrap-icons/font/bootstrap-icons.css"],//追加
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="m-5">
+        <!-- <NuxtPage /> -->
+        <div class="btn btn-success">Success</div>
+        <div class="btn btn-danger">Danger</div>
+        <div class="btn btn-warning">Warning</div>
+    </div>
+</template>
