1875384265524fc06d8eff57d63320a006bbef1b
Kouta219 <104189576+Kouta219@users.noreply.github.com>
2023-11-07 03:20:01 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="m-5">
-        <!-- <NuxtPage /> -->
-        <div class="btn btn-success">Success</div>
-        <div class="btn btn-danger">Danger</div>
-        <div class="btn btn-warning">Warning</div>
+    <div class="card mx-auto" style="width: 18rem;">
+        <img src="../assets/MicrosoftTeams-image.png" class="mx-auto d-block" style="width: 100px; height: 100px;">
+        <div class="card-body">
+            <h3 class="card-title">LOGIN</h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+        </ul>
+        <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
     </div>
 </template>
