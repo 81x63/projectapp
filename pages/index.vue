08b54db318626b91c2c0854a0a8093f270b379cf
Kouta219 <104189576+Kouta219@users.noreply.github.com>
2023-11-07 12:02:55 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
             <label for="floatingPassword">Password</label>
         </div>
         <div class="card-body text-center mt-2">
-            <a href="#" class="btn bg-primary-subtle">ログイン</a>
+            <a href="../pages/set.vue" class="btn bg-primary-subtle">ログイン</a>
         </div>
     </div>
 </template>
